972a245e8446295b044f1fbc284a312146c7c009
rahimshahad <rahim.shahad2@gmail.com>
2021-09-17 13:09:40 -0400

    commit tree 8e43cc7c650c6cd08e4c117305f825fa132f7551

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" }
+    //...
+]
 
+
+function superbowlWin(Objects) {
+    let result = Objects.find((e) => {
+        return e["result"] === "W";
+    });
+
+    return !!result ? result["year"] : undefined
+}
